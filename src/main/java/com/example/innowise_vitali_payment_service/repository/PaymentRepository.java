8684a635b2257f72e9a4082d1759975f81c9bf1c
Vitali Artyomchik <pdijsj2992@gmail.com>
2026-06-23 14:18:44 +0300

    fix(JAC-22): resolve Liquibase config and repository aggregation issues

--- src/main/java/com/example/innowise_vitali_payment_service/repository/PaymentRepository.java
+++ src/main/java/com/example/innowise_vitali_payment_service/repository/PaymentRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PaymentRepository extends MongoRepository<Payment, String> {
+public interface PaymentRepository extends MongoRepository<Payment, String>, PaymentRepositoryCustom {
 }
