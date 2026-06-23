891713cad908fc2c84f54ffb7652eb1c58eebcf5
Vitali Artyomchik <pdijsj2992@gmail.com>
2026-06-23 13:04:48 +0300

    Initial commit

---
+++ src/main/java/com/example/innowise_vitali_payment_service/InnowiseVitaliPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.innowise_vitali_payment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnowiseVitaliPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnowiseVitaliPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/innowise_vitali_payment_service/InnowiseVitaliPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.innowise_vitali_payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnowiseVitaliPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
